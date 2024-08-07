e416f0ce54356b7e2e134fbd7be2263ac5e9bf1f
Arpan Chattopadhyay <arpanchattopadhyay23@gmail.com>
2024-08-07 14:42:31 +0530

    add basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,15 @@ import { Calendar } from '@/components/ui/calendar';
 
 export default function Home() {
   return (
-    <main className='h-screen flex flex-col items-center bg-gray-200'>
-      <Calendar />
+    <main className='h-screen grid place-items-center bg-gray-200'>
+      <div className='max-w-3xl h-[500px] flex'>
+        <div className='bg-white'>
+          <Calendar />
+        </div>
+        <div className='bg-primary w-2/4'>
+          <span className='text-white'>hello world</span>
+        </div>
+      </div>
     </main>
   );
 }
