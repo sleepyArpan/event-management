d141beb937c04f152a2c024c11f22f7b7e15ce99
Arpan Chattopadhyay <arpanchattopadhyay23@gmail.com>
2024-08-10 10:07:27 +0530

    move events listing logic to a different component

--- src/app/[date]/page.tsx
+++ src/app/[date]/page.tsx
@@ -1,9 +1,5 @@
+import { EventsList } from '@/components/events';
 import { ReactNode } from 'react';
-import Link from 'next/link';
-import { Pencil } from 'lucide-react';
-import { formatDate } from 'date-fns';
-import { buttonVariants } from '@/components/ui/button';
-import prisma from '@/lib/prisma';
 
 type EventsProps = {
   params: { date?: string };
@@ -18,59 +14,9 @@ function EventsContainer({ children }: { children: ReactNode }) {
 }
 
 export default async function Events({ params }: EventsProps) {
-  const { date } = params;
-
-  if (!date || isNaN(Number(date))) {
-    return (
-      <EventsContainer>
-        <span className='text-foreground font-medium'>
-          Please select a date from the{' '}
-          <Link
-            className='underline underline-offset-1 text-sky-600 font-medium'
-            href='/'>
-            calendar
-          </Link>{' '}
-          first
-        </span>
-      </EventsContainer>
-    );
-  }
-
-  const eventsByDate = await prisma.event.findMany({
-    where: { date: Number(date) },
-  });
-
   return (
     <EventsContainer>
-      {eventsByDate.length > 0 ? (
-        eventsByDate.map(event => (
-          <div key={event.id} className='flex gap-5 w-full'>
-            <span>{event.name}</span>
-            <div>
-              <Link href={`/edit-event/?eventId=${event.id}`}>
-                <Pencil className='text-sm' />
-              </Link>
-            </div>
-          </div>
-        ))
-      ) : (
-        <span className='text-foreground font-semibold leading-3'>
-          No events found for{' '}
-          {formatDate(new Date(Number(date)), 'dd MMM yyyy')},{' '}
-          <Link
-            className='underline underline-offset-1 text-sky-600 font-medium'
-            href={`/add-event/?date=${date}`}>
-            add events for this day
-          </Link>{' '}
-          or go back to the{' '}
-          <Link
-            className='underline underline-offset-1 text-sky-600 font-medium'
-            href='/'>
-            calendar
-          </Link>{' '}
-          to add more events for different days!
-        </span>
-      )}
+      <EventsList date={params.date} />
     </EventsContainer>
   );
 }
---
+++ src/components/events.tsx
@@ -0,0 +1,57 @@
+import Link from 'next/link';
+import { formatDate } from 'date-fns';
+import { Pencil } from 'lucide-react';
+import prisma from '@/lib/prisma';
+
+type EventsProps = {
+  date?: string;
+};
+
+export async function EventsList({ date }: EventsProps) {
+  if (!date || isNaN(Number(date))) {
+    return (
+      <span className='text-foreground font-medium'>
+        Please select a date from the{' '}
+        <Link
+          className='underline underline-offset-1 text-sky-600 font-medium'
+          href='/'>
+          calendar
+        </Link>{' '}
+        first
+      </span>
+    );
+  }
+
+  const eventsByDate = await prisma.event.findMany({
+    where: { date: Number(date) },
+  });
+
+  return eventsByDate.length > 0 ? (
+    eventsByDate.map(event => (
+      <div key={event.id} className='flex gap-5 w-full'>
+        <span>{event.name}</span>
+        <div>
+          <Link href={`/edit-event/?eventId=${event.id}`}>
+            <Pencil className='text-sm' />
+          </Link>
+        </div>
+      </div>
+    ))
+  ) : (
+    <span className='text-foreground font-semibold leading-3'>
+      No events found for {formatDate(new Date(Number(date)), 'dd MMM yyyy')},{' '}
+      <Link
+        className='underline underline-offset-1 text-sky-600 font-medium'
+        href={`/add-event/?date=${date}`}>
+        add events for this day
+      </Link>{' '}
+      or go back to the{' '}
+      <Link
+        className='underline underline-offset-1 text-sky-600 font-medium'
+        href='/'>
+        calendar
+      </Link>{' '}
+      to add more events for different days!
+    </span>
+  );
+}
