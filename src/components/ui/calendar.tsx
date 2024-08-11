c9acb6c6f39b7b5cc548e4001cacd42918ffa1c9
Arpan Chattopadhyay <arpanchattopadhyay23@gmail.com>
2024-08-11 22:50:16 +0530

    add custom day to render tags

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -3,8 +3,14 @@
 import * as React from 'react';
 import { useRouter } from 'next/navigation';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
-import { DayPicker, DropdownProps } from 'react-day-picker';
-
+import {
+  DayPicker,
+  DropdownProps,
+  Day,
+  useDayPicker,
+  useActiveModifiers,
+} from 'react-day-picker';
+import { endOfDay, startOfDay, isToday } from 'date-fns';
 import { buttonVariants } from '@/components/ui/button';
 import {
   Select,
@@ -20,6 +26,51 @@ import { cn } from '@/lib/utils';
 export type CalendarProps = React.ComponentProps<typeof DayPicker> & {
   events: Array<Event>;
 };
+
+type CustomDayProps = {
+  displayMonth: Date;
+  date: Date;
+  events?: Array<Event>;
+};
+
+type TagProps = {
+  event: Event;
+  date: Date;
+};
+
+function Tag({ event, date }: TagProps) {
+  return (
+    <span
+      className={cn(
+        'hidden cursor-pointer md:block text-xs lg:text-sm font-medium rounded mb-2 relative whitespace-nowrap truncate px-2 bg-sky-500 text-white',
+        { 'bg-white text-sky-500': isToday(date) }
+      )}>
+      {event.name}
+    </span>
+  );
+}
+
+function CustomDay({ date, displayMonth, events }: CustomDayProps) {
+  const dayPicker = useDayPicker();
+  const activeModifiers = useActiveModifiers(date, displayMonth);
+  return (
+    <>
+      <Day date={date} displayMonth={displayMonth} />
+      {events?.length && events?.length > 0 ? (
+        <div className='rounded-full w-3 h-3 absolute -top-1 -right-1 bg-sky-400 text-white flex items-center justify-center z-20 md:invisible'>
+          <span className='text-xs'>{events?.length}</span>
+        </div>
+      ) : null}
+      <button
+        className='cursor-pointer items-center w-full h-[calc(100%-42px)] absolute bottom-0 left-1/2 -translate-x-1/2 px-1 overflow-hidden'
+        onClick={event => dayPicker.onDayClick?.(date, activeModifiers, event)}>
+        {events?.map(event => (
+          <Tag key={event.id} event={event} date={date} />
+        ))}
+      </button>
+    </>
+  );
+}
 
 function Calendar({ className, classNames, events, ...props }: CalendarProps) {
   const router = useRouter();
@@ -53,7 +104,7 @@ function Calendar({ className, classNames, events, ...props }: CalendarProps) {
           'w-11 md:w-[88px] lg:w-[120px] text-blue-500 rounded-md font-normal text-[0.8rem]',
         row: 'flex justify-evenly w-full mt-2',
         cell: 'h-9 w-9 md:w-20 md:h-20 lg:w-28 lg:h-28 text-sm p-0 relative',
-        day: 'w-9 h-9 md:h-20 md:w-20 lg:h-28 lg:w-28 font-normal text-blue-500 border rounded-md border-blue-500',
+        day: 'w-9 h-9 md:h-20 md:w-20 lg:h-28 lg:w-28 font-normal text-blue-500 border rounded-md border-blue-500 flex justify-center pt-1',
         day_today: 'bg-primary text-white',
         ...classNames,
       }}
@@ -95,6 +146,25 @@ function Calendar({ className, classNames, events, ...props }: CalendarProps) {
                 </ScrollArea>
               </SelectContent>
             </Select>
+          );
+        },
+        Day: ({ date, displayMonth }) => {
+          const eventsOnDay = events.filter(event => {
+            const eventStartDate = new Date(event.startDate);
+            const eventEndDate = new Date(event.endDate);
+            const startOfSpecificDay = startOfDay(date).valueOf();
+            const endOfSpecificDay = endOfDay(date).valueOf();
+            return (
+              eventStartDate.valueOf() <= endOfSpecificDay &&
+              eventEndDate.valueOf() >= startOfSpecificDay
+            );
+          });
+          return (
+            <CustomDay
+              date={date}
+              displayMonth={displayMonth}
+              events={eventsOnDay}
+            />
           );
         },
       }}
