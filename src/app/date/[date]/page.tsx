a98583a82493e695b691838d5f6ce48785bf8f71
Arpan Chattopadhyay <arpanchattopadhyay23@gmail.com>
2024-08-11 20:20:56 +0530

    add date for dynamic date param page

--- src/app/date/[date]/page.tsx
+++ src/app/date/[date]/page.tsx
@@ -1,3 +1,4 @@
+import { formatDate } from 'date-fns';
 import { EventsList } from '@/components/events';
 
 type EventsProps = {
@@ -6,7 +7,13 @@ type EventsProps = {
 export default function Events({ params }: EventsProps) {
   return (
     <div className='p-4 md:w-[500px]'>
-      <h1 className='text-lg font-semibold mb-4'>Events for the day</h1>
+      <h1 className='text-lg font-semibold mb-4'>
+        Events for the day{' '}
+        {formatDate(
+          new Date(params.date ? Number(params.date) : ''),
+          'dd MMM yyyy'
+        )}
+      </h1>
       <EventsList date={params.date} />
     </div>
   );
